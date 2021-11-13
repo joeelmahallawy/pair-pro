1147d37f309f8c265295e7a592f0b091ce667c1d
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-11-12 17:25:36 -0800

    added find and update operations on DB

--- pages/api/mongo.ts
+++ pages/api/mongo.ts
@@ -13,7 +13,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (!db) throw Error("Could not connect to database");
     const users = db.collection("users");
     const matches = db.collection("matches");
-    // users.deleteMany({});
+    users.deleteMany({});
     // Fetch posts from typeform responses API  then store it to MongoDB
     if (req.method === "POST") getPostsThenStoreToDB(req, res, users);
     if (req.method === "GET") getUserFromDB(req, res, users);
