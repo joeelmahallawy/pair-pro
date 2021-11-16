21bcc9744493d1c10a22e6756e7906e08e42bf52
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2021-11-16 09:33:13 -0800

    added user queue findign

--- pages/api/mongo.ts
+++ pages/api/mongo.ts
@@ -45,9 +45,9 @@ async function postToQueue(
   res: NextApiResponse,
   DB: Collection
 ) {
-  const { id } = JSON.parse(req.body);
+  const id = JSON.parse(req.body);
   await DB.insertOne({ id });
-  res.json({ id });
+  res.json(id);
 }
 
 async function getUserFromDB(
